95efb592155af46dfa91a7d981a8e68ab0ad54b9
Houssam ELOUINKHIR <h.elouinkhir@gmail.com>
2021-06-04 23:54:24 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SelectAuto msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SelectAuto from './components/SelectAuto.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SelectAuto
   }
 }
 </script>
